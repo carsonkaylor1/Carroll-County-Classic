e821cc8f9f593c9e92589dc437cf36330a3f3536
carsonkaylor1 <carsonkaylor@gmail.com>
2022-07-30 15:51:12 -0400

    shirt size

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,24 +1,40 @@
 import React from 'react'
-import { Cell, Grid, Textfield} from 'react-mdl';
+import { Cell, Grid, Textfield } from 'react-mdl'
 
-class Player extends React.Component{
-
-    render(){
-        return(
-            <Grid className='player-grid'>
-                    <Cell col={4}>
-                        {this.props.label} Name{this.props.extraLabel}:
-                    </Cell>
-                    <Cell col={4}>
-                        <Textfield 
-                            name={this.props.name}
-                            onChange={this.props.onTermChange}
-                            label={this.props.label}
-                        />
-                    </Cell>
-            </Grid>
-        )
+class Player extends React.Component {
+  render() {
+    if (this.props.label == 'Team') {
+      return (
+        <Grid className="player-grid">
+          <Cell col={4}>
+            {this.props.label} Name{this.props.extraLabel}:
+          </Cell>
+          <Cell col={4}>
+            <Textfield
+              name={this.props.name}
+              onChange={this.props.onTermChange}
+              label={this.props.label}
+            />
+          </Cell>
+        </Grid>
+      )
+    } else {
+      return (
+        <Grid className="player-grid">
+          <Cell col={4}>
+            {this.props.label} Name + shirt size{this.props.extraLabel}:
+          </Cell>
+          <Cell col={4}>
+            <Textfield
+              name={this.props.name}
+              onChange={this.props.onTermChange}
+              label={this.props.label + ' + shirt size'}
+            />
+          </Cell>
+        </Grid>
+      )
     }
+  }
 }
 
-export default Player;
+export default Player
